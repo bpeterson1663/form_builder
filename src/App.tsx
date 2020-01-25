38d172c486ebb97310805f65632e9be4780aabbf
bpeterson1663 <bpeterson1663@gmail.com>
2020-01-25 13:46:17 -0600

    style(prettier): ran prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
-import { hot } from "react-hot-loader";
-import "./App.css";
-import Welcome from "./components/Welcome";
+import * as React from 'react'
+import { hot } from 'react-hot-loader'
+import './App.css'
+import Welcome from './components/Welcome'
 
 const App: React.FunctionComponent = (): JSX.Element => {
-  return (
-    <div className="App">
-      <h1>Form Builder</h1>
-      <Welcome firstName="Brady" lastName="Peterson" />
-    </div>
-  );
-};
+    return (
+        <div className="App">
+            <h1>Form Builder</h1>
+            <Welcome firstName="Brady" lastName="Peterson" />
+        </div>
+    )
+}
 
-export default hot(module)(App);
+export default hot(module)(App)
